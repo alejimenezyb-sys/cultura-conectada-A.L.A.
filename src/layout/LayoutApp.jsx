0fb7b8e4a06243b0c2dd743ad402d9c4ed935556
Ale Jiménez <alejimenez.yb@gmail.com>
2026-08-18 01:27:14 -0300

    Cambios en LayoutApp.jsx | Se añadieron las rutas correspondientes (Footer / Navbar )

---
+++ src/layout/LayoutApp.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router";
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+
+
+const LayoutApp = () => {
+  return (
+
+    <>
+    <Navbar />
+    <main>
+<Outlet />
+    </main>
+    
+    <Footer />
+    </>
+  )
+}
+
+export default LayoutApp
